a532d9a5ab9f59a287fd97798b5045b8dbd9b54a
S. MohammadMahdi Zamanian <76493524+MM25Zamanian@users.noreply.github.com>
2022-05-24 17:18:40 +0430

    feat(page-about): v1 (#13)

--- src/elements/page-about.ts
+++ src/elements/page-about.ts
@@ -1,7 +1,9 @@
+import {localize} from '@alwatr/i18n';
 import {css, html, CSSResultGroup} from 'lit';
 import {customElement} from 'lit/decorators/custom-element.js';
 
 import {AppElement} from '../app-debt/app-element';
+import globalStyleSheets from '../global.css';
 
 import type {ListenerInterface} from '@alwatr/signal';
 import type {TemplateResult} from 'lit';
@@ -21,12 +23,33 @@ declare global {
  */
 @customElement('page-about')
 export class PageAbout extends AppElement {
-  static override styles: CSSResultGroup = css`
-    :host {
-      display: flex;
-      flex-direction: column;
-    }
-  `;
+  static override styles: CSSResultGroup = [
+    globalStyleSheets,
+    css`
+      :host {
+        display: flex;
+        flex-direction: column;
+      }
+      .main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        padding: 1rem;
+      }
+      .main .about {
+        display: flex;
+        flex-direction: column;
+        background-color: #fff;
+        border-radius: 12px;
+        box-shadow: 0 12px 32px 0 #0003;
+        padding: 1rem;
+        min-width: 320px;
+      }
+    `,
+  ];
 
   protected _listenerList: Array<unknown> = [];
 
@@ -41,6 +64,13 @@ export class PageAbout extends AppElement {
   }
 
   protected override render(): TemplateResult {
-    return html`<h1>Page About</h1>`;
+    return html`
+      <div class="main">
+        <div class="about">
+          <h1>${localize('spy_game_web_app')}</h1>
+          <p>${localize('developer')}: mm25zamanian@gmail.com</p>
+        </div>
+      </div>
+    `;
   }
 }
