eee866cf98ffb182132b7bbb810a71ae4c422682
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-08-04 04:49:09 +0430

    chore: install capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import {CapacitorConfig} from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ir.mm25zamanian.spy',
+  appName: 'spy.mm25zamanian.ir',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
