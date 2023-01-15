9c685a2329988c7ac3c1b2c4d69e1186c8a331a6
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-01-16 02:27:35 +0330

    feat: remove extras & old features

---
+++ src/components/navigation-bar/navigation-tab.ts
@@ -0,0 +1,27 @@
+import {AlwatrDummyElement, css, html, customElement} from '@alwatr/element';
+
+import config from '../../config';
+
+import type {LitRenderType} from '../../types/lit-render';
+
+@customElement('navigation-tab')
+export class NavigationBar extends AlwatrDummyElement {
+  static override styles = [
+    config.styles,
+    css`
+      :host {
+        display: flex;
+      }
+    `,
+  ];
+
+  override render(): LitRenderType {
+    return html``;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'navigation-tab': NavigationBar;
+  }
+}
--- src/type.ts
+++
@@ -1,3 +0,0 @@
-import type {nothing, TemplateResult} from '@alwatr/element';
-
-export type LitRenderCallbackType = TemplateResult<1> | typeof nothing | Array<LitRenderCallbackType>;
