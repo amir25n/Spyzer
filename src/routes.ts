cd1995f9eb75b741470509a7affd8fbaf28dfc52
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-01-23 00:05:29 +0330

    feat(router): routes test

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,19 @@
+import {l10n} from '@alwatr/i18n';
 import {html} from 'lit';
 import {Routes} from './types/route';
 
 const routes: Routes = {
-  home: {
+  'home': {
     icon: 'home',
-    render: () =>
-      html`
-        <h1>Hello World</h1>
-        <alwatr-icon name="home" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="home-2" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="3d-rotate" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="3dcube" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="airplane" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="camera" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="convert" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="command" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="cpu" url-prefix="/iconsax/"></alwatr-icon>
-        <alwatr-icon name="woman" url-prefix="/iconsax/"></alwatr-icon>
-      `,
+    render: () => html`<h1>${l10n.localize('page_home')}</h1>`,
+  },
+  'game': {
+    icon: 'game',
+    render: () => html`<h1>${l10n.localize('page_game')}</h1>`,
+  },
+  'who-am-i': {
+    icon: 'user-square',
+    render: () => html`<h1>${l10n.localize('page_who_am_i')}</h1>`,
   },
 };
 
