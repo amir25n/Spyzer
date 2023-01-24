cae54403fd767e737e532cbc57dde82172e76f38
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-01-24 18:24:35 +0330

    feat(surface): component

--- src/components/surface/surface.ts
+++ src/components/surface/surface.ts
@@ -1,9 +1,4 @@
-import {
-  AlwatrDummyElement,
-  html,
-  css,
-  type CSSResultGroup,
-} from '@alwatr/element';
+import {AlwatrDummyElement, css, type CSSResultGroup} from '@alwatr/element';
 
 /**
  * Alwatr Surface Element
@@ -17,37 +12,37 @@ import {
  * @attr {Boolean} active-outline - outline on active and focus
  * @attr {Boolean} disabled
  *
- * @cssprop {String} [--_surface-color-on=var(--sys-color-on-surface-hsl)]
- * @cssprop {String} [--_surface-color-bg=var(--sys-color-surface-hsl)]
- * @cssprop {String} [--_surface-elevation=var(--sys-surface-elevation-0)]
- * @cssprop {String} [--_surface-tint-color=var(--sys-color-surface-tint-hsl)]
- * @cssprop {String} [--_surface-state-color=var(--_surface-color-on)]
- * @cssprop {Number} [--_surface-tint-opacity=0]
- * @cssprop {Number} [--_surface-state-opacity=0]
+ * @cssprop {String} [--surface-color-on=var(--sys-color-on-surface-hsl)]
+ * @cssprop {String} [--surface-color-bg=var(--sys-color-surface-hsl)]
+ * @cssprop {String} [--surface-elevation=var(--sys-surface-elevation-0)]
+ * @cssprop {String} [--surface-tint-color=var(--sys-color-surface-tint-hsl)]
+ * @cssprop {String} [--surface-state-color=var(--surface-color-on)]
+ * @cssprop {Number} [--surface-tint-opacity=0]
+ * @cssprop {Number} [--surface-state-opacity=0]
  */
 export class Surface extends AlwatrDummyElement {
   static override styles: CSSResultGroup = css`
     :host {
-      --_surface-color-on: var(--sys-color-on-surface-hsl);
-      --_surface-color-bg: var(--sys-color-surface-hsl);
-      --_surface-elevation: var(--sys-surface-elevation-0);
-      --_surface-tint-color: var(--sys-color-surface-tint-hsl);
-      --_surface-state-color: var(--_surface-color-on);
-      --_surface-tint-opacity: 0;
-      --_surface-state-opacity: 0;
+      --surface-color-on: var(--sys-color-on-surface-hsl);
+      --surface-color-bg: var(--sys-color-surface-hsl);
+      --surface-elevation: var(--sys-surface-elevation-0);
+      --surface-tint-color: var(--sys-color-surface-tint-hsl);
+      --surface-state-color: var(--surface-color-on);
+      --surface-tint-opacity: 0;
+      --surface-state-opacity: 0;
 
       display: block;
       padding: calc(2 * var(--sys-spacing-track));
-      color: hsl(var(--_surface-color-on));
-      background-color: hsl(var(--_surface-color-bg));
-      box-shadow: var(--_surface-elevation);
+      color: hsl(var(--surface-color-on));
+      background-color: hsl(var(--surface-color-bg));
+      box-shadow: var(--surface-elevation);
       background-image: linear-gradient(
-          hsla(var(--_surface-tint-color) / var(--_surface-tint-opacity)),
-          hsla(var(--_surface-tint-color) / var(--_surface-tint-opacity))
+          hsla(var(--surface-tint-color) / var(--surface-tint-opacity)),
+          hsla(var(--surface-tint-color) / var(--surface-tint-opacity))
         ),
         linear-gradient(
-          hsla(var(--_surface-state-color) / var(--_surface-state-opacity)),
-          hsla(var(--_surface-state-color) / var(--_surface-state-opacity))
+          hsla(var(--surface-state-color) / var(--surface-state-opacity)),
+          hsla(var(--surface-state-color) / var(--surface-state-opacity))
         );
       background-repeat: no-repeat;
       outline: 0;
@@ -63,35 +58,40 @@ export class Surface extends AlwatrDummyElement {
     }
 
     :host([filled]) {
-      --_surface-color-bg: var(--sys-color-surface-variant-hsl);
+      --surface-color-bg: var(--sys-color-surface-variant-hsl);
     }
 
     :host([elevated]) {
-      --_surface-elevation: var(--sys-surface-elevation-1);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-1);
+      --surface-elevation: var(--sys-surface-elevation-1);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-1);
     }
+
     :host([elevated='2']) {
-      --_surface-elevation: var(--sys-surface-elevation-2);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-2);
+      --surface-elevation: var(--sys-surface-elevation-2);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-2);
     }
+
     :host([elevated='3']) {
-      --_surface-elevation: var(--sys-surface-elevation-3);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-3);
+      --surface-elevation: var(--sys-surface-elevation-3);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-3);
     }
+
     :host([elevated='4']) {
-      --_surface-elevation: var(--sys-surface-elevation-4);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-4);
+      --surface-elevation: var(--sys-surface-elevation-4);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-4);
     }
 
     :host([stated]:hover) {
-      --_surface-state-opacity: var(--sys-surface-state-opacity-hover);
+      --surface-state-opacity: var(--sys-surface-state-opacity-hover);
     }
+
     :host([stated]:active) {
-      --_surface-state-opacity: var(--sys-surface-state-opacity-pressed);
+      --surface-state-opacity: var(--sys-surface-state-opacity-pressed);
     }
+
     :host([stated]:focus),
     :host([stated]:focus-within) {
-      --_surface-state-opacity: var(--sys-surface-state-opacity-focus);
+      --surface-state-opacity: var(--sys-surface-state-opacity-focus);
     }
 
     :host([active-outline]:active),
@@ -104,32 +104,36 @@ export class Surface extends AlwatrDummyElement {
     :host([stated]:hover:not(:active)),
     :host([stated]:focus),
     :host([stated]:focus-within) {
-      --_surface-elevation: var(--sys-surface-elevation-1);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-1);
+      --surface-elevation: var(--sys-surface-elevation-1);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-1);
     }
+
     :host([stated][elevated]:hover:not(:active)),
     :host([stated][elevated]:focus),
     :host([stated][elevated]:focus-within) {
-      --_surface-elevation: var(--sys-surface-elevation-2);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-2);
+      --surface-elevation: var(--sys-surface-elevation-2);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-2);
     }
+
     :host([stated][elevated='2']:hover:not(:active)),
     :host([stated][elevated='2']:focus),
     :host([stated][elevated='2']:focus-within) {
-      --_surface-elevation: var(--sys-surface-elevation-3);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-3);
+      --surface-elevation: var(--sys-surface-elevation-3);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-3);
     }
+
     :host([stated][elevated='3']:hover:not(:active)),
     :host([stated][elevated='3']:focus),
     :host([stated][elevated='3']:focus-within) {
-      --_surface-elevation: var(--sys-surface-elevation-4);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-4);
+      --surface-elevation: var(--sys-surface-elevation-4);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-4);
     }
+
     :host([stated][elevated='4']:hover:not(:active)),
     :host([stated][elevated='4']:focus),
     :host([stated][elevated='4']:focus-within) {
-      --_surface-elevation: var(--sys-surface-elevation-5);
-      --_surface-tint-opacity: var(--sys-surface-tint-opacity-5);
+      --surface-elevation: var(--sys-surface-elevation-5);
+      --surface-tint-opacity: var(--sys-surface-tint-opacity-5);
     }
 
     :host([disabled]) {
@@ -143,9 +147,4 @@ export class Surface extends AlwatrDummyElement {
       opacity: var(--sys-surface-disabled-outlined-opacity);
     }
   `;
-
-  override render(): unknown {
-    this._logger.logMethod('render');
-    return html`<slot></slot>`;
-  }
 }
