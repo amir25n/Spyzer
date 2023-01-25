e4ec94b069c313cc9bc0948b7545e373c2a3170b
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-01-25 20:52:54 +0330

    feat(page-who-am-i): base component

--- src/pages/page-who-am-i.ts
+++ src/pages/page-who-am-i.ts
@@ -1,11 +1,76 @@
-import {customElement} from '@alwatr/element';
+import {
+  AlwatrDummyElement,
+  css,
+  customElement,
+  html,
+  LocalizeMixin as localizeMixin,
+} from '@alwatr/element';
 
+import config from '../config';
 
-import {PageGame} from './page-game';
+import '../components/card-box/card-box';
+import '../components/button/outlined-button';
+
+import type {LitRenderType} from '../types/lit-render';
 
 @customElement('page-who-am-i')
-export class PageWhoAmI extends PageGame {
-  static override styles = PageGame.styles;
+export class PageWhoAmI extends localizeMixin(AlwatrDummyElement) {
+  static override styles = [
+    config.styles,
+    css`
+      :host {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        height: 100%;
+
+        padding: calc(2 * var(--sys-spacing-track));
+      }
+
+      card-box {
+        width: 100%;
+      }
+
+      card-box p {
+        margin: calc(30 * var(--sys-spacing-track)) 0;
+      }
+
+      card-box .buttons-row {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: calc(2 * var(--sys-spacing-track));
+        margin: calc(2 * var(--sys-spacing-track));
+      }
+    `,
+  ];
+
+  override render(): LitRenderType {
+    return html`
+      <card-box
+        header-text="من کی ام ؟"
+        header-icon="lamp-on"
+        header-icon-url-prefix="/iconsax/"
+        have-line
+      >
+        <p></p>
+        <div class="buttons-row">
+          <outlined-button
+            label=${this.l10n.localize('message-to-me')}
+            icon="sms-tracking"
+            url-prefix="/iconsax/"
+          ></outlined-button>
+          <outlined-button
+            label=${this.l10n.localize('add-word')}
+            icon="add"
+            url-prefix="/iconsax/"
+          ></outlined-button>
+        </div>
+      </card-box>
+    `;
+  }
 }
 
 declare global {
